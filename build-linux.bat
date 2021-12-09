d849ccd96863346839370aa9cb3ecb35dd8ba5d0
xmdhs <xmdhss@gmail.com>
2021-12-09 17:38:57 +0800

    fix

--- build-linux.bat
+++ build-linux.bat
@@ -3,4 +3,4 @@ SET GOOS=linux
 SET GOARCH=amd64
 set CC=x86_64-linux-musl-gcc
 set CXX=x86_64-linux-musl-g++
-go build -trimpath -ldflags "-w -s -linkmode \"external\"" 
+go build -trimpath -ldflags "-w -s -linkmode \"external\" -extldflags \"-static\"" 
